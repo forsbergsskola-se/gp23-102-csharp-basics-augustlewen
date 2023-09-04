395340a7ab9dfeff425f3a03c4f8ceb8a535b924
augustlewen <augustlewen@live.com>
2023-09-04 16:15:29 +0200

    P6: added variables and output

--- P6DataTypes/Program.cs
+++ P6DataTypes/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string outputString = "Hello World";
+Console.WriteLine(outputString);
+
+float decimalValue = 0.3f;
+Console.WriteLine(decimalValue);
+
+bool isAwesome = true;
+Console.WriteLine(isAwesome);
+
+byte hexByte = 0xa7;
+Console.WriteLine(hexByte);
