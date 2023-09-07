bb840b44aba35eca3b3ce6bb3c94fc2aa17136e8
augustlewen <augustlewen@live.com>
2023-09-07 13:39:32 +0200

    P10: Completed the assignment

--- P10Boolean/Program.cs
+++ P10Boolean/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("What is your age");
+int age = int.Parse(Console.ReadLine());
+
+bool isChild = age >= 0 && age <= 12;
+bool isTeenager = age > 12 && age <= 19;
+bool isGrownup = age > 19;
+
+
+Console.WriteLine("You are a Child: " + isChild);
+Console.WriteLine("You are a Teenager: " + isTeenager);
+Console.WriteLine("You are a Grownup: " + isGrownup);
