f15af9b59fde586c1763fdda6dbc9334f37696ca
augustlewen <augustlewen@live.com>
2023-09-07 14:05:04 +0200

    P11: Completed

--- P11IfElse/Program.cs
+++ P11IfElse/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("What's your age");
+int age = int.Parse(Console.ReadLine());
+
+string ageGroup;
+
+if (age <= 12)
+    ageGroup = "Child";
+else if (age <= 19)
+    ageGroup = "Teenager";
+else
+    ageGroup = "Grownup";
+
+Console.WriteLine("You are a " + ageGroup);
+
+Console.WriteLine("Give me another integer");
+int value = int.Parse(Console.ReadLine());
+
+int largerValue = age > value ? age : value;
+
+Console.WriteLine("The maximum is: " + largerValue);
+Console.WriteLine(largerValue % 2 == 0 ? "Your number is an even number" : "Your number is an odd number");
