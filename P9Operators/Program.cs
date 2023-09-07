20bf8995582edd4418e8d4cc31a0e3af79d8a5f7
augustlewen <augustlewen@live.com>
2023-09-07 11:08:14 +0200

    P9: Completed the assignment

--- P9Operators/Program.cs
+++ P9Operators/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Give me a number of seconds");
+string input = Console.ReadLine();
+
+float floatValue = float.Parse(input);
+
+float total = floatValue / (60 * 60 * 24);
+
+int seconds = (int)(floatValue % 60);
+int minutes =(int)((floatValue / 60) % 60);
+int hours = (int)((floatValue / (60 * 60)) % 24);
+int days = (int)(total);
+
+
+Console.WriteLine("Seconds:" + seconds);
+Console.WriteLine("Minutes:" + minutes);
+Console.WriteLine("Hours:" + hours);
+Console.WriteLine("Days:" + days);
+
+Console.WriteLine($"{days} : {hours} : {minutes} : {seconds}");
+Console.WriteLine("In total, that's " + total);
