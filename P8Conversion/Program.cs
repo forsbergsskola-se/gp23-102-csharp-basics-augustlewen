bd5b320c6c757a06fc8cae514b4e85e1b1ba6afb
augustlewen <augustlewen@live.com>
2023-09-07 09:29:22 +0200

    P8: Added input, conversion and output

---
+++ P8Conversion/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Give me a number.");
+string input = Console.ReadLine();
+double dValue = Convert.ToDouble(input);
+
+Console.WriteLine(dValue);
+int iValue = Convert.ToInt32(dValue);
+Console.WriteLine(iValue);
+int iBrokenValue = Convert.ToInt32(input);
+Console.WriteLine(iBrokenValue);
