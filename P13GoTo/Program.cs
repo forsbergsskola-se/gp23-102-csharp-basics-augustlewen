01163059a6394c7f43add8bb79ba7792d53780d9
augustlewen <augustlewen@live.com>
2023-09-13 13:23:14 +0200

    P13: Completed assignment

--- P13GoTo/Program.cs
+++ P13GoTo/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+
+int number = random.Next(1, 101);
+    
+Console.WriteLine("I have picked a number (1-100). It's your turn to guess it!");
+
+AskForNumber:
+int input = int.Parse(Console.ReadLine());
+
+if (input != number)
+{
+    if(input < number)
+        Console.WriteLine("Nope! My number is Greater!");
+    else
+        Console.WriteLine("Nope! My number is Smaller!");
+
+    goto AskForNumber;
+}
+
+Console.WriteLine("That's the number! Well played!");
